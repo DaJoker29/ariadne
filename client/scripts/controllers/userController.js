1358a318648ad33e025f8de1d813e4624cbd4937
Dewitt Buckingham <dj729@comcast.net>
2016-02-11 14:49:48 -0800

    Add Google Authentication

--- client/scripts/controllers/userController.js
+++ client/scripts/controllers/userController.js
@@ -1,8 +1,28 @@
 angular
     .module('ariadne')
-    .controller('UserController', function () {
+    .controller('UserController', function ($firebaseArray, $firebaseAuth, $log) {
 
+
+        var ref = new Firebase('https://ariadne-todo.firebaseio.com'); //eslint-disable-line no-undef
+        var auth = $firebaseAuth(ref);
         var vm = this;
 
-        vm.name = 'User';
+        vm.authenticated = false;
+
+        vm.signIn = function () {
+            auth.$authWithOAuthPopup('google').then(function ( data ) {
+                vm.user = data;
+                vm.authenticated = true;
+                $log.log('Authenticated', data);
+            }).catch( function( err ) {
+                $log.log('Error:', err);
+            });
+        };
+
+        vm.logOut = function () {
+            auth.$unauth();
+            vm.authenticated = false;
+            vm.user = null;
+            $log.log('UnAuthenticated');
+        };
     });
