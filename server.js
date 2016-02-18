3ab3387cfe945f07c621b6184007e5f82be1d33c
Dewitt Buckingham <dj729@comcast.net>
2016-02-18 02:05:52 -0800

    Password Hashing
    Finally!

--- server.js
+++ server.js
@@ -112,7 +112,14 @@ function verifyCredentials ( username, password, done ) {
     User.findOne( { username: username }, function (err, user) {
         if( err ) { return done( err ); }
         if( !user ) { return done(null, false); }
-        if( user.password !== password) { return done(null, false); }
-        return done(null, user);
+        authController.verifyPassword( password, user.password,
+            function ( err, flag ) {
+                if(err) { return done(err); }
+                if (flag) {
+                    return done(null, user);
+                } else {
+                    return done(null, false);
+                }
+            });
     })
 }
