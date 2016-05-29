15a5087ae57638fd4c233d63c7c26a8279074e9f
Dewitt Buckingham <dj729@comcast.net>
2016-05-29 16:36:23 -0700

    Throng (Clusters)

--- bin/api.js
+++ bin/api.js
@@ -1,1 +1,12 @@
-console.log('API online');
+const throng = require('throng');
+
+throng(id => {
+  console.log(`Started worker ${id}`);
+
+  process.on('SIGTERM', () => {
+    console.log(`Worker ${id} exiting`);
+    console.log('Cleaning up');
+    process.exit();
+  });
+});
+// console.log('API online');
