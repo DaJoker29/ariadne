334a1ea9381f4f674aaebc62a7d027d501ff61a8
Dewitt Buckingham <dj729@comcast.net>
2016-05-30 23:34:25 -0700

    Morgan (Logging)

--- server/ariadne.js
+++ server/ariadne.js
@@ -1,17 +1,59 @@
+/* eslint-disable global-require*/
 module.exports = (id) => {
-  /* eslint-disable global-require */
-  const express = require('express');
-  const app = express();
-  /* eslint-enable global-require */
+  /**
+   * Modules
+   *
+   */
 
-  // Variables
-  const port = process.env.SERVER_PORT || 3000;
+  const express = require('express');
+  const morgan = require('morgan');
+  const fs = require('fs');
+  const path = require('path');
+
+  /**
+   * Variables
+   *
+   */
+
+  const app = express();
+  const PORT = process.env.SERVER_PORT || 3000;
+  const LOG_FORMAT = process.env.LOG_FORMAT || 'combined';
+
+  /**
+   * Environment Settings
+   *
+   */
+
+  if (process.env.NODE_ENV === 'development') {
+    /**
+     * Development Settings
+     */
+
+    app.use(morgan(LOG_FORMAT));
+  } else {
+    /**
+     * Production Settings
+     */
+
+    const accessLogStream = fs.createWriteStream(path.join(__dirname,
+      '..', 'access.log'), { flags: 'a' });
+
+    app.use(morgan(LOG_FORMAT, { stream: accessLogStream }));
+  }
+
+  /**
+   * Routes
+   */
 
   app.get('/', (req, res) => {
     res.send(`Worker #${id}`);
   });
 
-  app.listen(port, () => {
-    console.log(`Instance ${id} on port ${port}`);
+  /**
+   * Start
+   */
+
+  app.listen(PORT, () => {
+    console.log(`Instance ${id} on PORT ${PORT}`);
   });
 };
