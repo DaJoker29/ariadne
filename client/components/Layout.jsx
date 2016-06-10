cc057cb24e5746fdf76098f840b0882e39a6ea20
Dewitt Buckingham <dj729@comcast.net>
2016-06-09 21:50:00 -0700

    Meta Links
    Added logout and settings links

--- client/components/Layout.jsx
+++ client/components/Layout.jsx
@@ -4,6 +4,11 @@ import { Link } from 'react-router';
 const Layout = ({ children }) => (
   <div className="container">
     <div className="page-header">
+      <div className="meta pull-right">
+        <Link to="/settings">Settings</Link>
+        &nbsp;&bull;&nbsp;
+        <a href="/logout">Log Out</a>
+      </div>
       <h1><Link to="/">Ariadne</Link> <small>Intelligent Productivity</small></h1>
     </div>
       {children}
