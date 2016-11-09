e20913dd2de3134d0f40135256759ca52901ee8a
Dewitt Buckingham <dj729@comcast.net>
2016-11-08 18:35:50 -0800

    Another one
    They don't want us to have a pool bigger than Kanye's.

--- scripts/shell/upgrade.sh
+++ scripts/shell/upgrade.sh
@@ -3,12 +3,31 @@
 # Checks the repository for upgrades and restarts the server if tests are passed.
 #
 
-git fetch origin
-if [ `git rev-list HEAD...origin/master --count` != 0 ] ; then
-  echo "Up to date"
+# git fetch origin
+# if [ `git rev-list HEAD...origin/master --count` != 0 ] ; then
+#   echo "Up to date"
+# else
+#   echo "Fetching"
+#   git pull
+#   npm test
+#   pm2 reload ariadne
+# fi
+
+
+UPSTREAM=${1:-'@{u}'}
+LOCAL=$(git rev-parse @)
+REMOTE=$(git rev-parse "$UPSTREAM")
+BASE=$(git merge-base @ "$UPSTREAM")
+
+if [ $LOCAL = $REMOTE ]; then
+    echo "Up-to-date"
+elif [ $LOCAL = $BASE ]; then
+    echo "Need to pull"
+    git pull
+    npm test
+    pm2 reload ariadne
+elif [ $REMOTE = $BASE ]; then
+    echo "Need to push"
 else
-  echo "Fetching"
-  git pull
-  npm test
-  pm2 reload ariadne
+    echo "Diverged"
 fi
