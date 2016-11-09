62dc10de8460979cdfca427ccb9fee31c9135078
Dewitt Buckingham <dj729@comcast.net>
2016-11-08 18:14:52 -0800

    Fix Typo

--- scripts/shell/upgrade.sh
+++ scripts/shell/upgrade.sh
@@ -2,6 +2,7 @@
 #
 # Checks the repository for upgrades and restarts the server if tests are passed.
 #
+
 git fetch origin
 if [ "$(git rev-parse HEAD)" -ne "$(git rev-parse @{u})" ] ; then
   echo "Fetching"
