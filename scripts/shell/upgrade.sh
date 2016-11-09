0717cd487af9317c7e25e817618c98d6a9b70f43
Dewitt Buckingham <dj729@comcast.net>
2016-11-08 18:47:37 -0800

    Code Cleanup

--- scripts/shell/upgrade.sh
+++ scripts/shell/upgrade.sh
@@ -3,17 +3,6 @@
 # Checks the repository for upgrades and restarts the server if tests are passed.
 #
 
-# git fetch origin
-# if [ `git rev-list HEAD...origin/master --count` != 0 ] ; then
-#   echo "Up to date"
-# else
-#   echo "Fetching"
-#   git pull
-#   npm test
-#   pm2 reload ariadne
-# fi
-#
-
 if [ $(git rev-parse HEAD) = $(git ls-remote $(git rev-parse --abbrev-ref @{u} | \
 sed 's/\// /g') | cut -f1) ]
 then
