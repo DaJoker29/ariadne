792dc42911ba410ae6db61d4de359f92099e23f8
Dewitt Buckingham <dj729@comcast.net>
2016-11-11 17:37:29 -0800

    Add Code Coverage

--- test/twitter.js
+++ test/twitter.js
@@ -1,19 +1,59 @@
 const assert = require('assert');
+const twitter = require('../scripts/twitter.js');
 
 describe('Twitter', () => {
-  it('should fail if configuration file does not exist', () => {
-    assert.throws(() => {
-      // eslint-disable-next-line global-require
-      require('./twitter/no-config-file.js');
+  // before(() => {
+  //   twitter.init();
+  // });
+
+  describe('init()', () => {
+    it('should fail if no callback is specified', () => {
+      assert.throws(twitter.init, /No Callback Provided/);
     });
   });
-
-  it('should fail if no configuration values are provided', (done) => {
-    // eslint-disable-next-line global-require
-    const twitter = require('./twitter/no-credentials.js');
-    twitter.init((err) => {
-      assert.equal(err.message, 'No Twitter API credentials.');
-      done();
+  describe('tweet()', () => {
+    it('should fail if no message is provided', (done) => {
+      twitter.tweet(null, (err) => {
+        assert.deepEqual(err, Error('No Message Sent'));
+        done();
+      });
+    });
+    it('should throw an exception if it fails with no callback provided', () => {
+      assert.throws(() => {
+        twitter.tweet();
+      }, /Failure with no callback/);
+    });
+  });
+  describe('attach()', () => {
+    it('should fail if no callback is specified', () => {
+      assert.throws(() => {
+        twitter.attach('testing');
+      }, /No callback to return/);
+    });
+    it('should fail if no command argument is given', () => {
+      assert.throws(() => {
+        twitter.attach(() => {});
+      }, /No command to watch/);
+    });
+    it('should fail if command is an empty string', () => {
+      assert.throws(() => {
+        twitter.attach('', () => {});
+      }, /No command to watch/);
+    });
+    it('should fail if command is multiple words', () => {
+      assert.throws(() => {
+        twitter.attach('Some description', () => {});
+      }, /No command to watch/);
+    });
+    it('should succeed if no description or usage is specified', () => {
+      twitter.attach('desc', () => {});
+      const commands = twitter.commands();
+      assert.ok(commands.find(e => 'desc' === e.command));
+    });
+    it('should succeed if no usage is specified', () => {
+      twitter.attach('usage', 'Some description', () => {});
+      const commands = twitter.commands();
+      assert.ok(commands.find(e => 'usage' === e.command));
     });
   });
 });
