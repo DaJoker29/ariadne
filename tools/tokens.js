b70de6f3ecd4f1f08b2427090930d660c0748f9c
Dewitt Buckingham <dj729@comcast.net>
2016-11-19 15:40:46 -0800

    remove Access

--- tools/tokens.js
+++ tools/tokens.js
@@ -4,6 +4,7 @@ const program = require('commander');
 
 program
   .command('create <name>')
+  .alias('add')
   .description('register a new app and log an access token')
   .action((name) => {
     db.createApp(name, (err, data) => {
@@ -26,6 +27,21 @@ program
         console.log(`Something went wrong.\n${err}`);
       } else {
         console.log(`New access key generated for '${data.appName}'\n${data.token}\n`);
+      }
+      db.close();
+    });
+  });
+
+program
+  .command('remove <name>')
+  .alias('rm')
+  .description('Remove an apps access to the repository')
+  .action((name) => {
+    db.removeApp(name, (err, data) => {
+      if (err) {
+        console.log(`Something went wrong.\n${err}`);
+      } else {
+        console.log(`Access removed for '${data.appName}'\n`);
       }
       db.close();
     });
